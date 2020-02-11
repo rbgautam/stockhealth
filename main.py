aea1ad25ecb2d0ce85b1f238e5f42a3cb74222a3
Rahul Gautam <RGautam@iaai.com>
2020-02-11 16:02:47 -0600

    Added logic for reading csv

--- main.py
+++ main.py
@@ -1,51 +1,22 @@
 import yfinance as yf
+import pandas
+colnames = ['SYMBOL', 'BUY_PRICE', 'COUNT']
+datacsv = pandas.read_csv('stocks.csv', names=colnames, header=1)
 
-msft = yf.Ticker("TSLA")
-# get stock info
-res_dict = msft.info
-# res_string = res_string.replace("'","""")
-# print(res_dict["longBusinessSummary"])
-recommend = msft.recommendations
-# print(recommend) 
-hist = msft.history(period="6mo")
-# print(hist)
-earn = msft.earnings
-# print(earn)
-calendar = msft.calendar
-print(calendar)
-data = yf.download(  # or pdr.get_data_yahoo(...
-        # tickers list or string as well
-        tickers = "SPY AAPL MSFT",
 
-        # use "period" instead of start/end
-        # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
-        # (optional, default is '1mo')
-        period = "5d",
+# Stock names = 
+ticker_list = datacsv.SYMBOL.tolist()
 
-        # fetch data by interval (including intraday if period < 60 days)
-        # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
-        # (optional, default is '1d')
-        interval = "1d",
+print(ticker_list)
 
-        # group by ticker (to access via data['SPY'])
-        # (optional, default is 'column')
+data = yf.download(  tickers = ticker_list,    
+        period = "3mo",
+        interval = "5d",
         group_by = 'ticker',
-
-        # adjust all OHLC automatically
-        # (optional, default is False)
         auto_adjust = True,
-
-        # download pre/post regular market hours data
-        # (optional, default is False)
         prepost = True,
-
-        # use threads for mass downloading? (True/False/Integer)
-        # (optional, default is True)
         threads = True,
-
-        # proxy URL scheme use use when downloading?
-        # (optional, default is None)
         proxy = None
     )
 
-# print(data)
+print(data)
